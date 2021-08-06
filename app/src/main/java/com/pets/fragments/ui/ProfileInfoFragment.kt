e04170604ae4c2d692b789b56765d1c8f924c3e0
atiQkhalid <39992488+atiQkhalid@users.noreply.github.com>
2021-08-06 23:08:25 +0200

    gallary

--- app/src/main/java/com/pets/fragments/ui/ProfileInfoFragment.kt
+++ app/src/main/java/com/pets/fragments/ui/ProfileInfoFragment.kt
@@ -1,9 +1,11 @@
 package com.pets
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,6 +14,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import com.app.onlive.utils.FileUriUtils
+import com.permissionx.guolindev.PermissionX
 import com.pets.base.BaseFragment
 import com.pets.core.utils.Const.IS_IMAGE_VIDEO
 import com.pets.databinding.FragmentProfileInfoBinding
@@ -75,8 +79,22 @@ class ProfileInfoFragment : BaseFragment(), View.OnClickListener {
     }
 
 
+    @SuppressLint("InlinedApi")
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun openGallery(){
+        PermissionX.init(mainActivity)
+            .permissions(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            .request { allGranted, grantedList, deniedList ->
+                pickFromGallery(IS_IMAGE_VIDEO)
+            }
+    }
 
     var selection: String = ""
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     private fun pickFromGallery(selectionType: String = IS_IMAGE_VIDEO) {
         selection = selectionType
         Intent(Intent.ACTION_GET_CONTENT).apply {
@@ -89,27 +107,25 @@ class ProfileInfoFragment : BaseFragment(), View.OnClickListener {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     private val selectionFromGalleryResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val filePath =
                     result?.data?.data?.let { FileUriUtils.getRealPath(mainActivity, it) } ?: ""
                 val file = File(filePath)
-
+                binding.ivProfilePic.setImageURI(Uri.fromFile(file))
             }
         }
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.iv_profilePic -> pickFromGallery()
-            R.id.addImage -> pickFromGallery()
+            R.id.iv_profilePic -> openGallery()
+            R.id.addImage -> openGallery()
             R.id.tv_date -> clickDataPicker()
             R.id.male -> male()
             R.id.female -> female()
         }
-
-
     }
-
 }
